3e3c62cea443443df507a799ac628854142101b5
unknown <ezekuiel100@gmail.com>
2025-02-14 11:05:56 -0300

    first commit

---
+++ token.go
@@ -0,0 +1,26 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"  // TOKEN THAT WE DONT KNOW
+	EOF = "EOF" // END OF FILE
+
+	IDENT = "IDENT" 
+	INT = "INT"
+
+	//operators
+	ASSIGN = "=" 
+	PLUS = "+"
+
+	COMMA = "," 
+    SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
+
