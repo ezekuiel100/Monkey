62e20c4bbaa69291e3bb3fa112729b3e0ae0ec36
unknown <ezekuiel100@gmail.com>
2025-02-15 10:18:04 -0300

    read identifier

--- lexer.go
+++ lexer.go
@@ -50,6 +50,14 @@ func (l *Lexer) nextToken() token.Token {
 	case 0: 
 	    tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch){
+			tok.Literal = L.readIdentifier()
+			return tok
+		}else {
+			tok = newToken(token.ILLEGAL)
+		}
+
 	}
 
  l.readChar()
@@ -59,4 +67,16 @@ func (l *Lexer) nextToken() token.Token {
 
 func newToken(tokenType token.TokenType , ch byte) token.Token {
 	return token.Token{Type: tokenType , Literal: string(ch) } 
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := L.position 
+	
+	for isLetter(l.ch){
+		l.readChar()
+	}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' !! 'A' <= ch && <= 'Z' || ch == '_' 
 }
