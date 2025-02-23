fd9901bcba1489d66e22c6f98b7d1a492a790241
unknown <ezekuiel100@gmail.com>
2025-02-23 18:25:56 -0300

    methods expectPeek , curTokenIs and peekTokenIs

--- parser/parser.go
+++ parser/parser.go
@@ -73,3 +73,20 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 
 	return stmt
 }
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
