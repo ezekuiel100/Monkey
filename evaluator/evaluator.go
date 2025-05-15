d3a04726c465039124462c43f2305257e0ebf73a
unknown <ezekuiel100@gmail.com>
2025-05-15 15:49:15 -0300

    o extend the evalPrefixExpression function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -51,6 +51,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return NULL
 	}
