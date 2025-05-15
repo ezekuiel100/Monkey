5157efc5adfbab3886bd0b8611b0041f71a27742
unknown <ezekuiel100@gmail.com>
2025-05-15 18:31:27 -0300

    evalMinusPrefixOperatorExpression function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -70,3 +70,12 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 		return FALSE
 	}
 }
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+}
